1d70c42d1ae5c4fe32b9f8bec8265a230889be2f
cXiaof <310257509@qq.com>
2022-12-16 16:14:07 +0800

    add tip Panel in demo

--- demo/main.js
+++ demo/main.js
@@ -49,6 +49,12 @@ identifyTool.on('identify', (e) => {
   renderHitData(e.data)
 })
 
+const renderHitData = (data) => {
+  const layerLight = map.getLayer('hit')
+  const copys = data.map((geo) => geo.copy())
+  layerLight.clear().addGeometry(copys)
+}
+
 // prepare random data for testing
 const extent = map.getExtent()
 const min = extent.getMin()
@@ -62,11 +68,45 @@ for (var i = 0; i < 100; i++) {
 }
 layerData.addGeometry(markers)
 
-// support func in this demo
-const renderHitData = (data) => {
-  const layerLight = map.getLayer('hit')
-  const copys = data.map((geo) => geo.copy())
-  layerLight.clear().addGeometry(copys)
-}
+// new Toolbar
+const center = map.getCenter()
+const centers = [center.add(-0.01, 0), center, center.add(0.01, 0)]
+const radius = [500, 1000, 2000]
+const toolbar = new maptalks.control.Toolbar({
+  position: 'top-left',
+  items: [
+    {
+      item: 'setCenter',
+      children: centers.map((item, index) => ({
+        item: `center ${index + 1}`,
+        click: () => identifyTool.setCenter(item),
+      })),
+    },
+    {
+      item: 'setRadius',
+      children: radius.map((item) => ({
+        item: `${item}m`,
+        click: () => identifyTool.setRadius(item),
+      })),
+    },
+  ],
+}).addTo(map)
 
-window.iT = identifyTool
+// new tip Panel
+new maptalks.control.Panel({
+  position: 'bottom-left',
+  draggable: true,
+  custom: false,
+  content: `
+        Drag the blue dot on the right side of the range circle to see <br />
+        drag effect, and the hit geo will be highlighted after dragend.<br />
+        Click the options in setCenter and setRadius in toolbar to see <br />
+        the effect of trigger manual API setting parameters.<br />
+        <br />
+        拖动范围圆圈右侧的蓝色小点查看拖拽效果，放开后被命中的图形
+        <br />会高亮。<br />
+        点击左上角toolbar的setCenter和setRadius中的选项查看手动API<br />
+        设置参数的效果。
+    `,
+  closeButton: true,
+}).addTo(map)
